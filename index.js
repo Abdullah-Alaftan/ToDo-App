02bdbe48bca702b1cbbb74ab46a9bc8f05151a12
Abdullah Alaftan <as.alaftan@gmail.com>
2024-02-21 15:02:21 +0300

    renderd to do list on screen

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //starting up
+const todos = ["clean the apartment", "go to the gym"];
+const form = document.querySelector(".todo-form");
+const ul = document.createElement("ul");
+form.appendChild(ul);
+
+todos.forEach((elemnt) => {
+  const li = document.createElement("li");
+  li.textContent = elemnt;
+  ul.appendChild(li);
+});
