4d28052c1ff72d555e1ed0d376dd80298b0294e9
Abdullah Alaftan <as.alaftan@gmail.com>
2024-02-27 13:59:32 +0300

    added checkbox

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ let todos = [
     description: "i have been trying workkkkkk please",
   },
 ];
+
 const form = document.querySelector(".todo-form");
 const ul = document.createElement("ul");
+
 function rendertodos(array) {
   const todoContainer = document.querySelector(".container");
+
   ul.classList.add("todos");
   ul.innerHTML = "";
   todoContainer.appendChild(ul);
+
   array.forEach((task, index) => {
     const li = document.createElement("li");
     const title = document.createElement("span");
     const description = document.createElement("p");
     const deletebtn = document.createElement("button");
-    const editbtn = document.createElement("button"); 
+    const editbtn = document.createElement("button");
+    const checkbox = document.createElement("input"); 
     deletebtn.textContent = "delete";
-    editbtn.textContent = "edit"; 
+    editbtn.textContent = "edit";
+    checkbox.type = "checkbox"; 
     deletebtn.addEventListener("click", (e) => {
       const selecttodo = title.textContent;
       removetodo(selecttodo);
@@ -35,12 +41,21 @@ function rendertodos(array) {
         rendertodos(todos);
       }
     });
+    checkbox.addEventListener("change", (e) => {
+      if (checkbox.checked) {
+        title.style.textDecoration = "line-through";
+        description.style.textDecoration = "line-through";
+      } else {
+        title.style.textDecoration = "none";
+        description.style.textDecoration = "none";
+      }
+    });
     li.classList.add("todo");
     title.textContent = task.title;
     description.textContent = task.description;
-    li.append(title, description);
+    li.append(checkbox, title, description); 
     li.appendChild(deletebtn);
-    li.appendChild(editbtn); 
+    li.appendChild(editbtn);
     ul.appendChild(li);
   });
 }
@@ -52,13 +67,34 @@ function addtodo() {
   const span = document.createElement("span");
   const description = document.createElement("p");
   const deletebtn = document.createElement("button");
+  const editbtn = document.createElement("button"); 
+  const checkbox = document.createElement("input"); 
   deletebtn.textContent = "delete";
+  editbtn.textContent = "edit"; 
+  checkbox.type = "checkbox"; 
   deletebtn.addEventListener("click", (e) => {
     e.preventDefault();
     const selecttodo = span.textContent;
     removetodo(selecttodo);
   });
-  //
+  editbtn.addEventListener("click", (e) => {
+    const newTitle = prompt("Enter new title");
+    const newDescription = prompt("Enter new description");
+    if (newTitle && newDescription) {
+      const index = todos.findIndex(todo => todo.title === span.textContent && todo.description === description.textContent);
+      todos[index] = { title: newTitle, description: newDescription };
+      rendertodos(todos);
+    }
+  });
+  checkbox.addEventListener("change", (e) => {
+    if (checkbox.checked) {
+      span.style.textDecoration = "line-through";
+      description.style.textDecoration = "line-through";
+    } else {
+      span.style.textDecoration = "none";
+      description.style.textDecoration = "none";
+    }
+  });
   todos.push({
     title: value,
     description: valueDescription,
@@ -66,9 +102,9 @@ function addtodo() {
   li.classList.add("todo");
   span.textContent = value;
   description.textContent = valueDescription;
-  li.appendChild(span);
-  li.appendChild(description);
+  li.append(checkbox, span, description); 
   li.appendChild(deletebtn);
+  li.appendChild(editbtn); 
   todoContainer.appendChild(li);
 }
 const addtodobtn = document.querySelector("#submit-btn");
