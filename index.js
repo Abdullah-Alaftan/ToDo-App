444cbfeabb284f659d2017d30e220a0007891c15
Abdullah Alaftan <as.alaftan@gmail.com>
2024-02-27 11:22:39 +0300

    add descreption

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let todos = ["clean the apartment", "go to the gym"];
+let todos = [
+  {
+    title: "plese work",
+    description: "ihave been trying work please",
+  },
+  {
+    title: "plssss",
+    description: "i have been trying workkkkkk please",
+  },
+];
+
 const form = document.querySelector(".todo-form");
 const ul = document.createElement("ul");
 function rendertodos(array) {
@@ -10,51 +20,59 @@ function rendertodos(array) {
 
   array.forEach((task) => {
     const li = document.createElement("li");
-    const span = document.createElement("span");
+    const title = document.createElement("span");
+    const description = document.createElement("p");
     const deletebtn = document.createElement("button");
     deletebtn.textContent = "delete";
     deletebtn.addEventListener("click", (e) => {
       //   const value = e.target.parentNode.querySelector("span").textContent;
-      const selecttodo = e.target.previousSibling.textContent;
+      const selecttodo = title.textContent;
       console.log(selecttodo);
       removetodo(selecttodo);
     });
     li.classList.add("todo");
-    span.textContent = task;
-    ul.appendChild(li);
-    li.appendChild(span);
+    title.textContent = task.title;
+    description.textContent = task.description;
+    li.append(title, description);
     li.appendChild(deletebtn);
+    ul.appendChild(li);
   });
 }
 
 function addtodo() {
   const todoContainer = document.querySelector(".todos");
   const value = document.querySelector(".todo-input").value;
+  const valueDescription = document.querySelector(".todo-description").value;
 
   const li = document.createElement("li");
 
   const span = document.createElement("span");
+  const description = document.createElement("p");
   const deletebtn = document.createElement("button");
   deletebtn.textContent = "delete";
 
   deletebtn.addEventListener("click", (e) => {
     e.preventDefault();
-    const selecttodo = e.target.previousSibling.textContent;
+    const selecttodo = span.textContent;
     console.log(selecttodo);
     removetodo(selecttodo);
   });
   //
-  todos.push(value);
+  todos.push({
+    title: value,
+    description: valueDescription,
+  });
 
-  // send the li tag to HTML
   li.classList.add("todo");
   span.textContent = value;
+  description.textContent = valueDescription;
   li.appendChild(span);
+  li.appendChild(description);
   li.appendChild(deletebtn);
   todoContainer.appendChild(li);
 }
 const addtodobtn = document.querySelector("#submit-btn");
-console.log("addtodobtn:", addtodobtn);
+
 addtodobtn.addEventListener("click", function (e) {
   console.log("submitting.....");
   addtodo();
@@ -62,9 +80,10 @@ addtodobtn.addEventListener("click", function (e) {
   e.preventDefault();
 });
 function removetodo(selecttodo) {
-  const filteredtodo = todos.filter((todo) => {
-    return todo !== selecttodo;
+  const filteredtodo = todos.filter((todos) => {
+    return todos.title !== selecttodo;
   });
+  console.log(filteredtodo);
   todos = filteredtodo;
   rendertodos(filteredtodo);
 }
